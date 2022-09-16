793c147b2e30f65c3c11e41d1487f1fa9772cf13
ToQuery <toquery@qq.com>
2022-09-16 15:55:47 +0800

    init

---
+++ src/main/java/io/github/toquery/example/spring/security/jwe/ExampleSpringSecurityJweApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.security.jwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringSecurityJweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringSecurityJweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/security/jwe/ExampleSpringSecurityJweApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.security.jwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringSecurityJweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
